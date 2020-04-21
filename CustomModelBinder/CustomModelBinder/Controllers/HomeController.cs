1c07994261c47fbaa472c93e9e5814573475ae24
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-04-21 11:07:10 +0300

    Add point model binder.

---
+++ CustomModelBinder/CustomModelBinder/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CustomModelBinder.Models;
+using System.Collections.Generic;
+
+namespace CustomModelBinder.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private static List<Point> points = new List<Point>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {   
+            return View(points);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Point p)
+        {
+            points.Add(p);
+            p.Serialize();      
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            });
+        }
+
+    }
+}
---
+++ CustomModelBinder/CustomModelBinder/Infrastructure/CustomPointBinder.cs
@@ -0,0 +1,32 @@
+﻿using CustomModelBinder.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Threading.Tasks;
+
+namespace CustomModelBinder.Infrastructure
+{
+    public class CustomPointBinder : IModelBinder
+    {
+        private readonly IModelBinder fallbackBinder;
+
+        public CustomPointBinder(IModelBinder fallbackBinder)
+        {
+            this.fallbackBinder = fallbackBinder;
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+            var coordValues = bindingContext.ValueProvider.GetValue("Coordinates");
+            if (coordValues == ValueProviderResult.None)
+            {
+                return fallbackBinder.BindModelAsync(bindingContext);
+            }
+            bindingContext.Result = ModelBindingResult.Success(Point.Parse(coordValues.FirstValue));
+            return Task.CompletedTask;
+        }
+    }
+}
