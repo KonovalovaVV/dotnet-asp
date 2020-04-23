5c5ea7a7bbbcb23dfbb0c4582551a50be610ace6
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-04-23 11:10:21 +0300

    Add configuries to startup.

--- CustomModelBinder/CustomModelBinder/Startup.cs
+++ CustomModelBinder/CustomModelBinder/Startup.cs
@@ -22,6 +22,18 @@ namespace CustomModelBinder
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+                
             app.UseRouting();
 
             app.UseAuthorization();
