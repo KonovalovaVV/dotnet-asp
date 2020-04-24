eeddf3a24d04f5a2b96cf4004071a9944337ec0b
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-04-24 14:18:32 +0300

    Fix paging and searching.

--- RateLimit/Controllers/ProfileController.cs
+++ RateLimit/Controllers/ProfileController.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RateLimit.Services;
+using X.PagedList;
 
 namespace RateLimit.Controllers
 {
     public class ProfileController: Controller
     {
-        public IActionResult Profiles()
+        public object Profiles(int? page, int? pageSize, string searchString)
         {
-            return View(ProfileService.GetProfiles("Birthday", 1,null));
+            var profiles = ProfileService.GetProfiles("FirstName", 1, searchString);
+
+            IPagedList onePageOfProducts = profiles.ToPagedList(page ?? 1, pageSize ?? 2);
+            ViewBag.OnePageOfProducts = onePageOfProducts;
+
+            return View();
         }
     }
 }
--- RateLimit/Services/ProfileService.cs
+++ RateLimit/Services/ProfileService.cs
@@ -1,5 +1,4 @@
-﻿using PagedList;
-using RateLimit.Models;
+﻿using RateLimit.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,7 +8,28 @@ namespace RateLimit.Services
 {
     public class ProfileService
     {
-        public static IPagedList<ProfileViewModel> GetProfiles(string sortField, int? page, string searchString)
+        public static IQueryable<ProfileViewModel> GetProfiles(string sortField, int? page, string searchString)
+        {
+            var profiles = ReadProfiles();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                profiles = profiles.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstName.Contains(searchString)).ToList();
+            }
+
+            profiles = sortField switch
+            {
+                "Birthday" => profiles.OrderBy(s => s.Birthday).ToList(),
+                "LastName" => profiles.OrderBy(s => s.LastName).ToList(),
+                "FirstName" => profiles.OrderBy(s => s.FirstName).ToList(),
+                _ => profiles.OrderBy(s => s.Id).ToList(),
+            };
+
+            return profiles.AsQueryable();
+        }
+
+        private static List<ProfileViewModel> ReadProfiles()
         {
             List<ProfileViewModel> profiles;
             using (StreamReader r = new StreamReader("profiles.json"))
@@ -18,31 +38,7 @@ namespace RateLimit.Services
                 profiles = JsonSerializer.Deserialize<List<ProfileViewModel>>(json);
             }
 
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                profiles = profiles.Where(s => s.LastName.Contains(searchString)
-                                       || s.FirstName.Contains(searchString)).ToList();
-            }
-
-            switch (sortField)
-            {
-                case "Birthday":
-                    profiles = profiles.OrderBy(s => s.Birthday).ToList();
-                    break;
-                case "LastName":
-                    profiles = profiles.OrderBy(s => s.LastName).ToList();
-                    break;
-                case "FirstName":
-                    profiles = profiles.OrderBy(s => s.FirstName).ToList();
-                    break;
-                default:
-                    profiles = profiles.OrderBy(s => s.Id).ToList();
-                    break;
-            }
-
-            int pageSize = 1;
-            int pageNumber = (page ?? 1);
-            return profiles.ToPagedList(pageNumber, pageSize);
+            return profiles;
         }
     }
 }
